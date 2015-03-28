02b7e61be5b763e09cd78f42f22c59474c8dd288
Kyle <kyle.steven.brown@gmail.com>
2015-03-28 13:05:25 -0500

    intial commit

--- pelicanconf.py
+++ pelicanconf.py
@@ -20,7 +20,7 @@ CATEGORY_FEED_ATOM = None
 TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
-
+USER_LOGO_URL = SITEURL + '/static/images/index.jpg'
 # Blogroll
 #LINKS = (,)
 
